c3e2c33a6b699d979a683d607fb93d5448b08544
Noah Kennedy <noahkennedy@Noahs-MacBook-Pro.local>
2022-02-07 21:08:01 -0500

    Removing credentials

--- api_exploration/Beckett_ebay_test.py
+++ api_exploration/Beckett_ebay_test.py
@@ -3,12 +3,13 @@ from ebaysdk.finding import Connection as finding_connection
 
 
 # api = Connection(config_file='ebay.yaml')
-api = merchandising_connection(appid='Beckett86-b7b6-45a9-ae8a-8734862ecdd', config_file=None)
+beckett_appid = 'Beckett86-b7b6-45a9-ae8a-8734862ecdd'
+api = merchandising_connection(appid=beckett_appid, config_file=None)
 most_watched = api.execute('getMostWatchedItems', {})
 # print(most_watched)
 # print(most_watched.reply.itemRecommendations.item)
 
-api = finding_connection(appid='Beckett86-b7b6-45a9-ae8a-8734862ecdd', config_file=None, siteid="EBAY-US")
+api = finding_connection(appid=beckett_appid, config_file=None, siteid="EBAY-US")
 payload = {
         'keywords': 'Baseball', 
         'categoryId': ['212'],
@@ -24,3 +25,4 @@ print(response.reply.searchResult)
 
 for response in response.reply.searchResult.item:
     print(response.title) 
+    
